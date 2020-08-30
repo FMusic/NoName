6af2d864e186a53b93020271c01644cdee5e4643
frane <frane.music@gmail.com>
2020-08-30 18:07:27 +0200

    init
    Xamarin Forms project init

---
+++ NoName-Management/NoName-Management/NoName-Management/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using NoName_Management.Services;
+using NoName_Management.Views;
+
+namespace NoName_Management
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ NoName-Management/NoName-Management/NoName-Management/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using NoName_Management.Models;
+
+namespace NoName_Management.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
