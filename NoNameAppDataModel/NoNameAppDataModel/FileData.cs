94bcd0ba84a577fea7c7534b4ed3543d8b8c38ad
ms579 <milan.siklic@racunarstvo.hr>
2020-09-18 16:48:38 +0200

    Updated CSS. Fixed erroneous project path.

---
+++ NoNameAppDataModel/NoNameAppDataModel/FileData.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace NoNameAppDataModel
+{
+    [System.Serializable]
+    public class FileData
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("fileTimestamp")]
+        public string FileTimestamp { get; set; }
+
+        [JsonProperty("contentType")]
+        public string ContentType { get; set; }
+
+        [JsonProperty("data")]
+        public string Data { get; set; }
+    }
+}
---
+++ NoNameAppDataModel/NoNameAppDataModel/MainData.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace NoNameAppDataModel
+{
+    [System.Serializable]
+    public class MainData
+    {
+        [JsonProperty("categories")]
+        public List<Category> Categories { get; set; }
+
+        [JsonProperty("products")]
+        public List<Product> Products { get; set; }
+    }
+}
